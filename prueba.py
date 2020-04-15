172afbe59dbc22438168e220e003b5f514a8322a
MsAlderson <cerquera.e@gmail.com>
2020-04-14 21:10:31 -0500

    commit en la otra rama y sigo lo que dice esa guia rapida

--- prueba.py
+++ prueba.py
@@ -1,3 +1,12 @@
 print(2 + 2)
 
 print ("a ver qué pasa con tro commit")
+
+#cambiaré a la rama master, y guardaré ahí a ver si aparece el cambio en la otra cuenta
+print (2 * 9)
+
+#volveré a la nueva rama y haré algo con lo del merge upstream/master
+print ("hola hola")
+
+#desde la línea 5 se había borrado y no sé por qué ._. lo que sigue si va a la otra rama
+print ("tarola")
